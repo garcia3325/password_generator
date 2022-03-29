7db7b304791d75ebf26b89def49222f225888695
garcia3325 <garcia3325@gmail.com>
2022-03-29 10:59:40 -0400

    added arrays for password criteria

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //Variables
 var length =""
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var upperCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialCharacters = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-"~", "*", "?", ":"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// user interaction prompts
-// window.confirm("Would you like to use upper and lower cases?");
-// window.confirm("Would you like to use Numbers?");
-// window.confirm("Would you like to use special characters?");
-// window.prompt("Enter a number between 8 and 128 to set your new password's length")
+let upperCase = new Array ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+let lowerCase = new Array ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+let  numbers = new Array (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+let specialCharacters = new Array ( "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":" );
+
+// user interaction prompts for password criteria
+//window.prompt("Enter a number between 8 and 128 to set your password's length")
+//window.confirm("Would you like to use upper and lower cases?");
+//window.confirm("Would you like to use Numbers?");
+//window.confirm("Would you like to use special characters?");
+
+
+// validate input by ensuring that character length is between 18-128 and one input is selected
+
 
 
 
